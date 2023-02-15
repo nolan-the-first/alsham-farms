78a69e9bd6921dfcc42da6030f0e9808506cae41
Abdullrahman <white.nolan33@gmail.com>
2023-02-15 18:15:15 +0300

    fixed typo

--- AppContext.tsx
+++ AppContext.tsx
@@ -2,6 +2,14 @@ import { createContext } from "react";
 import { GlobalProps } from "./public/Assets/Types/types";
 
 const contextInitialValues: GlobalProps = {
+  facebook_link: "facebook.com",
+  instagram_link: "instagram.com",
+  linkedin_link: "linkedin.com",
+  address: "lorem ipsum",
+  phone_number: "0123456789",
+  email_address: "name@doamin.tld",
+  footer_license: "Copyright © 2023 • Al-Sham Farms",
+  screenWidth: 0,
   isArabic: null,
 };
 
