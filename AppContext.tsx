eabf27f6cc0bbb10e1e4d73049a057db5e87665e
Abdullrahman <white.nolan33@gmail.com>
2023-03-20 20:02:15 +0300

    fixed typo

--- AppContext.tsx
+++ AppContext.tsx
@@ -4,11 +4,12 @@ import { GlobalProps } from "./public/Assets/Types/types";
 const contextInitialValues: GlobalProps = {
   facebook_link: "facebook.com",
   instagram_link: "instagram.com",
-  linkedin_link: "linkedin.com",
   address: "lorem ipsum",
   phone_number: "0123456789",
   email_address: "name@doamin.tld",
   footer_license: "Copyright © 2023 • Al-Sham Farms",
+  tiktok_link: "https://www.tiktok.com/@alshamfarms.ae?_t=8ao4nNnAxGR&_r=1",
+  whatsapp_number: "+0123456789",
   screenWidth: 0,
   isArabic: null,
 };
